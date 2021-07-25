71930e0710592022f1897857bf8f3ec8b2f6d981
DoJiSeon <djs6620@gmail.com>
2021-07-25 13:47:14 +0900

    프로젝트 파일 업로드

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI관련 라이브러리
+using UnityEngine.SceneManagement; //씬 관리 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;//생존 시간
+    private bool isGameover;//게임오버 상태
+
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+    void Update()
+    {
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+        }
+        else
+        {
+            //게임오버 상태에서 R 키를 누른 경우
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if(surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+        recordText.text = "Best Time: " + (int)bestTime;
+    }
+}
